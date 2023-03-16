193b056acae2c7dcbf3cafc509829bb423e44b9d
joaogois <joao.f.gois@tecnico.ulisboa.pt>
2023-03-16 18:28:08 +0000

    message cj«hecker function and header

---
+++ node_protocols.c
@@ -0,0 +1,69 @@
+#include "node_protocols.h"
+
+int messagecheck(char buffer[], char** args){
+    int index = 0;
+    int n = -1;
+    char msg[10];
+
+    //divide buffer into tokens
+    //extract the first token
+    char* token = strtok(buffer, " \n");
+    if(token != NULL){
+        strcpy(msg, token);
+    }
+    //loop through the string to extract all other tokens
+    for(int i=0; token != NULL; i++ ) {
+        token = strtok(NULL, " \n");
+        args[i] = token;
+        n++;
+    }
+
+    //do all checks
+    if(strcmp(msg, MSGS[0])==0){
+        if(n!=3){
+            fprintf(stderr, "%s error: wrong number of arguments.\n", MSGS[0]);
+            index = -1;
+        }else{
+            index = 0;
+        }
+    }else if(strcmp(msg, MSGS[1])==0){
+        if(n!=3){
+            fprintf(stderr, "%s error: wrong number of arguments.\n", MSGS[0]);
+            index = -1;
+        }else{
+            index = 1;
+        }
+    }else if(strcmp(msg, MSGS[2])==0){
+        if(n!=1){
+            fprintf(stderr, "%s error: wrong number of arguments.\n", MSGS[0]);
+            index = -1;
+        }else{
+            index = 2;
+        }
+    }else if(strcmp(msg, MSGS[3])==0){
+        if(n!=3){
+            fprintf(stderr, "%s error: wrong number of arguments.\n", MSGS[0]);
+            index = -1;
+        }else{
+            index = 3;
+        }
+    }else if(strcmp(msg, MSGS[4])==0){
+        if(n!=3){
+            fprintf(stderr, "%s error: wrong number of arguments.\n", MSGS[0]);
+            index = -1;
+        }else{
+            index = 4;
+        }
+    }else if(strcmp(msg, MSGS[5])==0){
+        if(n!=3){
+            fprintf(stderr, "%s error: wrong number of arguments.\n", MSGS[0]);
+            index = -1;
+        }else{
+            index = 5;
+        }
+    }else{
+        fprintf(stderr, "error: %s is not a valid message.\n", msg);
+        return -1;
+    }
+    return index;
+}
